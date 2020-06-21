da72bae35d2e79f4139e71e30bddc0e7bc5286c0
greyhawktravelph <greyhawktravelph@gmail.com>
2020-06-21 16:06:58 +0800

    Upadte files

---
+++ header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Greyhawk Travel and Tours</title>
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+      <head>
+          <nav>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="">About Us</a></li>
+                <li><a href="tourpackages.php">Tour Packages</a></li>
+                <li><a href="">Contact Us</a></li>
+            </ul>
+          </nav>
+      </head>
